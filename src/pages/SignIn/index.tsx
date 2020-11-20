d3a1c78825280d758875cf6b705af093ccc1cd7f
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-19 22:20:57 -0300

    add login with api

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -14,13 +14,18 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useContext(AuthContext);
 
   const handleSubmit = useCallback(
-    async (data: object) => {
+    async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
         const schema = Yup.object().shape({
@@ -34,7 +39,10 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn();
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
       } catch (err) {
         const errors = getValidationErrors(err);
         formRef.current?.setErrors(errors);
