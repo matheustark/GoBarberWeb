0cdf232435e26ea1a878e626dd527d7952c97b1b
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-20 13:25:26 -0300

    creat a hook useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -50,3 +50,13 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used wothin an AuthProvider');
+  }
+
+  return context;
+}
